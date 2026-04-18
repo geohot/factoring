5f1ea08d5b00cbd158dae74c1ad3164b9db32c9c
George Hotz <geohot@gmail.com>
2026-04-18 10:47:17 +0800

    rename to N

--- qsieve.py
+++ qsieve.py
@@ -14,52 +14,61 @@ def gen_prime(bits):
 
 # generate
 SEMIPRIME_BITS = 18
+# generate number for factoring
+while 1:
+  p,q = gen_prime(SEMIPRIME_BITS), gen_prime(SEMIPRIME_BITS)
+  if p==q: continue
+  N = p*q
+  print(f"factoring {N} into {p} {q}")
+  break
+# no cheating
+del p,q
 
 # params for the solver
 B = 200
 
 # generate primes up to B
-PRIMES = [n for n in range(B+1) if is_prime(n)]
-NUM_RELATIONS = int(len(PRIMES)*1.2)+2
-print(f"num primes {len(PRIMES)}, num relations {NUM_RELATIONS}")
+FACTOR_BASE = [n for n in range(B+1) if is_prime(n)]
+NUM_RELATIONS = int(len(FACTOR_BASE)*1.2)+2
+print(f"num primes {len(FACTOR_BASE)}, num relations {NUM_RELATIONS}")
 
 def b_smooth_factorize(num):
-  factors = [0]*len(PRIMES)
-  for i,p in enumerate(PRIMES):
+  factors = [0]*len(FACTOR_BASE)
+  for i,p in enumerate(FACTOR_BASE):
     while num%p == 0:
       factors[i] += 1
       num //= p
       if num == 1: return factors
   return None
 
-def process_congruence(semiprime_factor_me, nums, factors):
+def process_congruence(N, nums, factors):
   # then we solve with GCD
-  rhs = math.prod((p**(f//2)) for p,f in zip(PRIMES, factors))
-  neg_factor = math.gcd(math.prod(nums) - rhs, semiprime_factor_me)
-  pos_factor = math.gcd(math.prod(nums) + rhs, semiprime_factor_me)
+  rhs = math.prod((p**(f//2)) for p,f in zip(FACTOR_BASE, factors))
+  neg_factor = math.gcd(math.prod(nums) - rhs, N)
+  pos_factor = math.gcd(math.prod(nums) + rhs, N)
 
   # try neg and pos factors
   for factor in [neg_factor, pos_factor]:
-    if factor == 1 or factor == semiprime_factor_me: continue
-    other_factor = semiprime_factor_me//factor
+    if factor == 1 or factor == N: continue
+    other_factor = N//factor
     print("FOUND")
     print(nums)
-    print([(p,f) for p,f in zip(PRIMES, factors) if f > 0])
+    print([(p,f) for p,f in zip(FACTOR_BASE, factors) if f > 0])
     print("factors into", factor, other_factor)
-    assert semiprime_factor_me == factor*other_factor
+    assert N == factor*other_factor
     return True
   return False
 
-def qsieve(semiprime_factor_me):
-  # first we need to find B-smooth numbers that are perfect squares
+def qsieve(N):
+  # first we need to find B-smooth Q(x) values
   # TODO: real qsieve doesn't check all of these, it finds likely candidates
-  start = math.isqrt(semiprime_factor_me)+1
-  end = math.isqrt(2*semiprime_factor_me)  # after this it gets dumb
+  start = math.isqrt(N)+1
+  end = math.isqrt(2*N)  # after this it gets dumb
   relations = []
   while len(relations) < NUM_RELATIONS and start < end:
     # TODO: chatgpt says it should be - and not % here, but that's slower
     # added end bound for the search to fix
-    qx = start*start - semiprime_factor_me
+    qx = start*start - N
     assert qx > 0
     relation = b_smooth_factorize(qx)
     if relation:
@@ -93,24 +102,16 @@ def qsieve(semiprime_factor_me):
     # if we get a hit, reconstruct nums and factors from combo
     if mask == 0:
       nums = []
-      factors = [0]*len(PRIMES)
+      factors = [0]*len(FACTOR_BASE)
       for j,(num,relation) in enumerate(relations):
         if combo&(1<<j):
           nums.append(num)
           for k,n in enumerate(relation):
             factors[k] += n
-      if process_congruence(semiprime_factor_me, nums, factors): break
+      if process_congruence(N, nums, factors): break
   else:
     raise RuntimeError("failed to find congruence")
 
 if __name__ == "__main__":
-  # generate number for factoring
-  while 1:
-    p,q = gen_prime(SEMIPRIME_BITS), gen_prime(SEMIPRIME_BITS)
-    if p==q: continue
-    semiprime_factor_me = p*q
-    print(f"factoring {semiprime_factor_me} into {p} {q}")
-    break
-
-  qsieve(semiprime_factor_me)
+  qsieve(N)
 
